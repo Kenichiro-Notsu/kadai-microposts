a59af72d0931deeece4399f7ca7b942f87c80f00
Kenichiro.Notsu <double.brain16@gmail.com>
2021-04-05 13:21:59 +0000

    follow,unfollow

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  
   root to: 'toppages#index'
 
   get 'login', to: 'sessions#new'
@@ -6,5 +7,13 @@ Rails.application.routes.draw do
   delete 'logout', to: 'sessions#destroy'
 
   get 'signup', to: 'users#new'
-  resources :users, only: [:index, :show, :create]
+  resources :users, only: [:index, :show, :create] do
+    member do
+      get :followings
+      get :followers
+    end
+  end
+
+  resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
 end
